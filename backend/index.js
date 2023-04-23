cea1f941bbb265e5badd34cab490c7280ea4ffe9
gaudincyprien <cyprien.gaudin@gmail.com>
2023-04-23 14:25:53 +0200

    ajout de mettre en favoris

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Autoriser les requêtes provenant uniquement de "http://localhost:3001"
+app.use(cors({
+    origin: 'http://localhost:3001',
+  }));
+  
+// Autoriser les requêtes provenant de tous les domaines, mais seulement les méthodes GET et POST
+app.use(cors({
+    methods: ['GET', 'POST']
+  }));
 
 // Configuration de la connexion à la base de données
 const connection = mysql.createConnection({
@@ -11,13 +27,27 @@ const connection = mysql.createConnection({
   database: 'citations'
 });
 
-// Endpoint pour récupérer une entré aléatoire de la 'citation'
+// Endpoint pour récupérer une entré aléatoire de la table'citation'
 app.get('/citation', function(req, res) {
   connection.query('SELECT * FROM citation', function(error, results, fields) {
-    if (error) throw error;
-    const random = Math.floor(Math.random() * results.length);
-    res.send(results[random]);
+        if (error) throw error;
+        const random = Math.floor(Math.random() * results.length);
+        res.send(results[random]);
   });
+});
+// Endpoint pour récupérer toutes les entrées de la table 'citation'
+app.get('/citations', function(req, res) {
+    connection.query('SELECT * FROM citation', function(error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+    });
+});
+// Endpoint pour insérer une nouvelle entrée dans la table 'citation'
+app.post('/ajoutcitation', function(req, res) {
+    connection.query('INSERT INTO citation SET ?', req.body, function(error, results, fields) {
+        if (error) throw error;
+        res.send(results);
+    });
 });
 
 // Lancement du serveur
--- src/components/Citation.jsx
+++ src/components/Citation.jsx
@@ -19,6 +19,7 @@ class Citation extends  Component {
         }
     }
     componentDidMount() {
+        console.log('componentDidMount');
         this.citationKaamelott()
     }
     citationKaamelott() {
@@ -30,7 +31,7 @@ class Citation extends  Component {
             console.log(error);
         });
     }
-    mesCitations() {
+    uneCitation() {
         axios.get('http://localhost:3000/citation')
         .then((result)=>{ 
             const resultat = {
@@ -52,13 +53,26 @@ class Citation extends  Component {
             console.log(error);
         });
     }
+    ajouterCitation() {
+        axios.post('http://localhost:3000/ajoutcitation', {
+            citation: this.state.citation.citation.citation,
+            auteur: this.state.citation.citation.infos.auteur,
+            acteur: this.state.citation.citation.infos.acteur,
+            personnage: this.state.citation.citation.infos.personnage,
+            saison: this.state.citation.citation.infos.saison,
+            episode: this.state.citation.citation.infos.episode,
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
     render() {
         return (
             <div id="citations">
                 <div id='citation'>
                     <p id='titre'>"{this.state.citation.citation.citation}"</p>
                     <p id='auteur'>{this.state.citation.citation.infos.personnage} - {this.state.citation.citation.infos.episode}</p>
-                    <div id='favoris'>
+                    <div id='favoris' onClick={() => this.ajouterCitation()}>
                         <img id='favorisimage'src='/favoris.png' alt=''/>
                         <p id='favoristext'>Mettre en favoris</p>
                     </div>
@@ -68,7 +82,7 @@ class Citation extends  Component {
                         <p id='autrecitation'>Afficher une autre citation</p>
                     </div>
                     <div>
-                        <button className='citation' onClick={() => this.mesCitations()}><img src='/yeux.png' alt=''/>Parmi mes citations</button>
+                        <button className='citation' onClick={() => this.uneCitation()}><img src='/yeux.png' alt=''/>Parmi mes citations</button>
                         <button className='citation' onClick={() => this.citationKaamelott()}><img src='/yeux.png' alt=''/>Parmi les citations Kaamlott</button>
                     </div>
                 </div>
