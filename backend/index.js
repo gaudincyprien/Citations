24b7edbf70ae05458643ccd95acbe30f36f6b089
gaudincyprien <cyprien.gaudin@gmail.com>
2023-04-23 21:05:56 +0200

    ajout base de donnee

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,58 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('citations', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql', // ou un autre dialecte de base de données
+  logging: false // désactiver les messages de journalisation SQL
+});
+
+class Citation extends Model {}
+Citation.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  citation: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  acteur: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  auteur: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  personnage: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  saison: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  episode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'citation'
+});
+
+sequelize.sync()
+  .then(() => {
+    console.log('Tables créées avec succès.');
+  })
+  .catch((erreur) => {
+    console.error('Erreur lors de la création des tables :', erreur);
+  });
 
 const app = express();
 
@@ -29,7 +81,7 @@ const connection = mysql.createConnection({
 
 // Endpoint pour récupérer une entré aléatoire de la table'citation'
 app.get('/citation', function(req, res) {
-  connection.query('SELECT * FROM citation', function(error, results, fields) {
+  connection.query('SELECT * FROM citations', function(error, results, fields) {
         if (error) throw error;
         const random = Math.floor(Math.random() * results.length);
         res.send(results[random]);
@@ -37,34 +89,36 @@ app.get('/citation', function(req, res) {
 });
 // Endpoint pour récupérer toutes les entrées de la table 'citation'
 app.get('/citations', function(req, res) {
-    connection.query('SELECT * FROM citation', function(error, results, fields) {
+    connection.query('SELECT * FROM citations', function(error, results, fields) {
         if (error) throw error;
         res.send(results);
     });
 });
 // Endpoint pour insérer une nouvelle entrée dans la table 'citation'
 app.post('/ajoutcitation', function(req, res) {
-    connection.query('INSERT INTO citation SET ?', req.body, function(error, results, fields) {
+    connection.query('INSERT INTO citations SET ?', req.body, function(error, results, fields) {
         if (error) throw error;
         res.send(results);
     });
 });
 // Endpoint pour supprimer une entrée de la table 'citation'
 app.delete('/supprimercitation/:id', function(req, res) {
-    connection.query('DELETE FROM citation WHERE id = ?', req.params.id, function(error, results, fields) {
+    connection.query('DELETE FROM citations WHERE id = ?', req.params.id, function(error, results, fields) {
         if (error) throw error;
         res.send(results);
     });
 });
 // Endpoint pour modifier une entrée de la table 'citation'
 app.put('/modifiercitation/:id', function(req, res) {
-    connection.query('UPDATE citation SET ? WHERE id = ?', [req.body, req.params.id], function(error, results, fields) {
+    connection.query('UPDATE citations SET ? WHERE id = ?', [req.body, req.params.id], function(error, results, fields) {
         if (error) throw error;
         res.send(results);
     });
 });
 
 // Lancement du serveur
-app.listen(3000, function() {
-  console.log('Serveur démarré sur le port 3000');
-});
+setTimeout(function() {
+    app.listen(3000, function() {
+      console.log('Serveur démarré sur le port 3000');
+    });
+  }, 5000);
